1cb36e811c5005217803a05c1361307725d379a0
hypergoat1273 <vaughanrees476@gmail.com>
2025-07-30 19:07:01 +0100

    Basic movement
    move

---
+++ Player/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+
+func _physics_process(_delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	
+	velocity = input_vector * 300
+	move_and_slide()
+	
